efc3491a6bc9ea78d98c63dec8f37846609afc5a
Juan Insuasti <juan.insuasti@gmail.com>
2019-11-29 10:06:13 -0500

    added player movement feature

---
+++ Star Figther/Assets/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Config
+    [SerializeField] float speed = 15f;
+    [SerializeField] float xPadding = 1.6f;
+    [SerializeField] float yPadding = 1.6f;
+
+    // State
+    float xMin;
+    float yMin;
+    float xMax;
+    float yMax;
+    
+
+    private void Start()
+    {
+        SetUpMoveBoundaries();
+    }
+
+    void Update()
+    {
+        MovePlayer();
+    }
+
+
+    void SetUpMoveBoundaries() {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - xPadding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - yPadding;
+    }
+
+    private void MovePlayer()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        var newXpos =  Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+
+        var deltaY= Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        var newYpos =  Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+
+        transform.position = new Vector2(newXpos, newYpos);
+    }
+}
