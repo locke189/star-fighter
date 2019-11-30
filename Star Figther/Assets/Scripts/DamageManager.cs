377fb8778ea37a37a2d63ba9cbfcae9546325ed8
Juan Insuasti <juan.insuasti@gmail.com>
2019-11-30 17:38:41 -0500

    created enemy shooter and damage manager

---
+++ Star Figther/Assets/Scripts/DamageManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageManager : MonoBehaviour
+{
+    [SerializeField] int healthPoints = 10;
+    [SerializeField] int damage = 10;
+    [SerializeField] bool isEnemyFire;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var friendlyFire = isEnemyFire == collision.GetComponent<DamageManager>().isEnemyFire;
+
+        if (!friendlyFire) {
+            DamageUpdate();
+            if (healthPoints <= 0)
+            {
+                DestructionSequence();
+            }
+        }
+    }
+
+    private void DestructionSequence()
+    {
+        Destroy(gameObject);
+    }
+
+    private void DamageUpdate()
+    {
+        healthPoints -= damage;
+    }
+}
---
+++ Star Figther/Assets/Scripts/EnemyShooter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooter : MonoBehaviour
+{
+    [SerializeField] float randomness = 0.5f;
+    // References
+    [SerializeField] GameObject laser;
+    [SerializeField] float laserXOffset = 0.8f;
+    [SerializeField] float laserYOffset = 1.6f;
+    [SerializeField] float laserSpeed = -1f;
+    [SerializeField] float laserDelay = 0.2f;
+    [SerializeField] float laserLifeTime = 3f;
+
+
+
+    IEnumerator Start() {
+        do
+        {
+            yield return StartCoroutine(ShootLaser());
+        } while (true);
+    }
+
+    IEnumerator ShootLaser()
+    {
+        Vector2 laserPos = new Vector2(transform.position.x + laserXOffset, transform.position.y + laserYOffset);
+        GameObject newLaser = Instantiate(laser, laserPos, transform.rotation);
+        newLaser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, laserSpeed);
+        Destroy(newLaser, laserLifeTime);
+        yield return new WaitForSeconds(CalculateRandomFireCadence());
+    }
+
+    float CalculateRandomFireCadence() {
+        var randomFactor = Random.Range(-laserDelay * randomness, laserDelay * randomness);
+        return laserDelay + randomFactor;
+    }
+}
